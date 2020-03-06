20629566885c32024d8c35ee50c1ef91db4bba39
bryanjiang04 <bryanjiang04@gmail.com>
2020-03-06 13:51:06 -0800

    Bounced Shapes

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -7,11 +7,11 @@ let r;
 
 function setup() {
   createCanvas(800, 400);
-  s = new square(0,200,20,"blue")
-  b = new Ball(0, 100,15,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,100,"green");
-  thirdBall = new Ball(500,40,50,"purple");
-  r = new square (400,50,70, "orange");
+  s = new square(0,200,20,"blue",4,5)
+  b = new Ball(0, 100,15,"red",4,5); //make a new ball from the Ball class and call it b.
+  anotherBall = new Ball(200,20,100,"green",4,5);
+  thirdBall = new Ball(500,40,50,"purple",4,5);
+  r = new square (400,50,70, "orange",4,5);
 }
 
 
@@ -27,18 +27,19 @@ function draw(){
     s.movesquare();
     r.drawsquare();
     r.movesquare();
-
 }
 
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y,size,color){ //every ball needs an x value and a y value
+	constructor(x,y,size,color,speed,speed2){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
         this.size=size;
+        this.speed=speed;
+        this.speed2=speed2;
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
@@ -46,17 +47,31 @@ class Ball {
 		    ellipse(this.x,this.y,this.size,this.size);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
+		this.x = this.x+this.speed;
+		this.y = this.y+this.speed2;
+    if (this.x == 800){
+      this.speed= -this.speed;
+    }
+    if (this.y == 400){
+      this.speed2= -this.speed2;
+    }
+    if (this.x == 0){
+      this.speed= -this.speed;
+    }
+    if (this.y == 0){
+      this.speed2= -this.speed2;
+    }
 	}
 }
 class square {
 
-	constructor(x,y,size,color){ //every ball needs an x value and a y value
+	constructor(x,y,size,color,speed,speed2){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
         this.size=size;
+        this.speed = speed;
+        this.speed2 = speed2;
 	}
 	drawsquare(){  // draw a ball on the screen at x,y
     		stroke(0);
@@ -64,7 +79,19 @@ class square {
 		    rect(this.x,this.y,this.size,this.size);
 	}
 	movesquare(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
-	}
+		this.x = this.x+this.speed;
+		this.y = this.y+this.speed2;
+  if (this.x == 800){
+    this.speed= -this.speed;
+  }
+  if (this.y == 400){
+    this.speed2= -this.speed2;
+  }
+  if (this.x == 0){
+    this.speed= -this.speed;
+  }
+  if (this.y == 0){
+    this.speed2= -this.speed2;
+  }
+  }
 }
